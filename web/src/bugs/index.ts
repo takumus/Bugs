289f9ab7880a34b84a758c5721ed9c43564eeff5
takumus <me@takumus.com>
2017-03-10 18:29:42 +0900

    add test render

--- web/src/bugs/index.ts
+++ web/src/bugs/index.ts
@@ -1,5 +1,22 @@
 export class Bug extends WORMS.Base {
+    private _graphics: PIXI.Graphics;
     constructor(length: number) {
         super(length);
+        this._graphics = new PIXI.Graphics();
+    }
+    public get graphics(): PIXI.Graphics {
+        return this._graphics;
+    }
+    public render() {
+        const g = this._graphics;
+        g.clear();
+        g.lineStyle(2, 0xff0000);
+        this.bone.forEach((pos, id) => {
+            if (id == 0) {
+                g.moveTo(pos.x, pos.y);
+            }else {
+                g.lineTo(pos.x, pos.y);
+            }
+        });
     }
 }
